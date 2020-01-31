a58d6959b7da863bb5f14eeaf315b7a13c72dd72
Doug Neal <doug@neal.me.uk>
2020-01-31 11:50:06 +0000

    Add logging to transform tool

--- fits_transform_tool.py
+++ fits_transform_tool.py
@@ -1,31 +1,67 @@
+import os
 import argparse
 import re
+import logging
+
 import tmad.fits
 from astropy.io import fits
 import astropy.visualization
 import imageio
 
+from tmad.logging import configure_logging
+
 
 def main():
     args = parse_arguments()
+    configure_logging(args.log_level)
+    logger = logging.getLogger()
 
+    # Interval and stretch functions could easily be made configurable
     transform = tmad.fits.TMAD_ACS_WFC_RAW_FITS_Transform(
         interval_function=tmad.fits.ZMaxInterval(),
         stretch_function=astropy.visualization.LogStretch(),
         scale=(0, 255),
     )
 
-    fits_data = fits.getdata(args.fits)
-    for index in range(transform.multiplier):
-        imageio.imwrite(
-            output_filename(args.fits, index),
-            transform(fits_data, index).astype('uint8'),
+    if args.fits_file:
+        fits_data = fits.getdata(args.fits_file)
+        for index in range(transform.multiplier):
+            output_filename = make_output_filename(
+                args.fits_file, index, args.output_dir,
+            )
+            imageio.imwrite(
+                output_filename,
+                transform(fits_data, index).astype('uint8'),
+            )
+
+    elif args.fits_dir:
+        dataset = tmad.fits.HSTImageDataset(
+            root_dir=args.fits_dir,
+            transform=transform,
         )
+        for image in dataset:
+            output_filename = make_output_filename(
+                image['src_filename'],
+                image['subimage_index'],
+                args.output_dir
+            )
+            logger.info('Exporting tile {0} from {1} to {2}'.format(
+                image['subimage_index'],
+                image['src_filename'],
+                output_filename,
+            ))
+            imageio.imwrite(
+                output_filename,
+                image['image_data'].astype('uint8'),
+            )
 
 
-def output_filename(input_filename, index):
+def make_output_filename(input_filename, index, output_dir):
     r = re.compile(r'\.fits', re.IGNORECASE)
-    return r.sub(f'.{index}.png', input_filename)
+    basename_in = os.path.basename(input_filename)
+    basename_out = r.sub(f'.{index}.png', basename_in)
+    absolute_out = os.path.join(output_dir, basename_out)
+    return absolute_out
 
 
 def parse_arguments():
@@ -33,11 +69,35 @@ def parse_arguments():
         description="FITS Transform Tool"
     )
 
-    parser.add_argument(
-        '--fits',
-        required=True,
+    input_selection = parser.add_mutually_exclusive_group()
+
+    input_selection.add_argument(
+        '--fits-file',
         type=str,
-        help='Input FITS file',
+        help='Single FITS file to process',
+    )
+
+    input_selection.add_argument(
+        '--fits-dir',
+        type=str,
+        help='Directory of FITS files to process',
+    )
+
+    parser.add_argument(
+        '--output-dir',
+        required=False,
+        type=str,
+        default=os.curdir,
+        help='Local folder to write images to (default: current directory)',
+    )
+
+    parser.add_argument(
+        '--log-level',
+        required=False,
+        type=str,
+        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+        default='INFO',
+        help='Log level (default: INFO)',
     )
 
     return parser.parse_args()
