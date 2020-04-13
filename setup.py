232c64185d03293f11cd13d3e014dfcc3cf37b72
Doug Neal <doug@neal.me.uk>
2020-04-13 16:54:03 +0100

    Pip packaging: add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='tmad-pytorch',
+    version='0.1',
+    url='https://github.com/dougneal/tmad-pytorch',
+    author='Doug Neal',
+    author_email='doug@neal.me.uk',
+    packages=['tmad'],
+)
